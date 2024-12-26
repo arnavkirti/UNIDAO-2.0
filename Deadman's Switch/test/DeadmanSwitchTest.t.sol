b0f11811cbef05007727f4a8499ac58191120314
Arnav Kirti <arnav.tkd@gmail.com>
2024-12-26 16:07:33 +0530

    feat: started testing

---
+++ Deadman's Switch/test/DeadmanSwitchTest.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import {Test} from "forge-std/Test.sol";
+import {DeadmanSwitch} from "../src/DeadmanSwitch.sol";
+
+contract DeadmanSwitchTest is Test {
+    DeadmanSwitch public deadmanswitch;
+    address public owner = address(0x1);
+    address public recipient = address(0x2);
+
+    function setUp() public {
+        vm.deal(owner, 10 ether);
+        vm.startPrank(owner);
+        deadmanswitch = new DeadmanSwitch{value: 1 ether}(recipient);
+        vm.stopPrank();
+    }
+
+    function testStillAliveUpdatesLastBlockAlive() public {
+        vm.startPrank(owner);
+        uint256 initialBlock = deadmanswitch.lastBlockAlive();
+        vm.roll(block.number + 1);
+        deadmanswitch.stillAlive();
+        assertEq(deadmanswitch.lastBlockAlive(), block.number);
+        vm.stopPrank();
+    }
+
+
+}
